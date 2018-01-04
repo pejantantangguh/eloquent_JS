d730da82e46c7b40d6c73d3444040a02f1e9380e
Pejantantangguh <suherman.ng@gmail.com>
2018-01-04 16:26:57 +1000

    adding growing function

--- app/index.js
+++ app/index.js
@@ -193,7 +193,63 @@ console.log(power(4, 3));
         return find(current + 5,"(" + history + "+ 5)") ||
                find(current * 3,"(" + history + "* 3)");
     }
-    return find(1,"1");
+    return find(1,1);
   }
   console.log(findSolution(13));
+
+  //This is how it works :
+  // To better understand how this function produces the effect we’re looking for, let’s look at all the calls to find that are made when searching for a solution for the number 13.
+  //
+  // find(1, "1")
+  //   find(6, "(1 + 5)")
+  //     find(11, "((1 + 5) + 5)")
+  //       find(16, "(((1 + 5) + 5) + 5)")
+  //         too big
+  //       find(33, "(((1 + 5) + 5) * 3)")
+  //         too big
+  //     find(18, "((1 + 5) * 3)")
+  //       too big
+  //   find(3, "(1 * 3)")
+  //     find(8, "((1 * 3) + 5)")
+  //       find(13, "(((1 * 3) + 5) + 5)")
+  //         found!
+  // The indentation suggests the depth of the call stack. The first time find is called it calls itself twice to explore the solutions that start with (1 + 5) and (1 * 3). The first call tries to find a solution that starts with (1 + 5) and, using recursion, explores every solution that yields a number less than or equal to the target number. Since it doesn’t find a solution that hits the target, it returns null back to the first call. There the || operator causes the call that explores (1 * 3) to happen. This search has more luck because its first recursive call, through yet another recursive call, hits upon the target number, 13. This innermost recursive call returns a string, and each of the || operators in the intermediate calls pass that string along, ultimately returning our solution.
+}
+
+{
+  //Growing functions
+  //http://eloquentjavascript.net/03_functions.html#h_eVDWIAuyBK
+
+  function printFarmInventory(cows,chickens){
+    //Cow digits counter
+    var cowString = String(cows);
+    while(cowString.length < 3)
+      cowString = "0" + cowString;
+    console.log(cowString + " Cows");
+    //Chicken Digits counter :
+    var chickenString = String(chickens)
+    while(chickenString.length < 3)
+      chickenString = "0" + chickenString;
+    console.log(chickenString + " Chickens");
+  }
+  printFarmInventory(10,5);
+
+}
+
+{
+    //Function to make it clean
+    function zeroPad(number, width) {
+    var string = String(number);
+    while (string.length < width)
+      string = "0" + string;
+    return string;
+  }
+
+  function printFarmInventory(cows, chickens, pigs) {
+    console.log(zeroPad(cows, 3) + " Cows");
+    console.log(zeroPad(chickens, 3) + " Chickens");
+    console.log(zeroPad(pigs, 3) + " Pigs");
+  }
+
+  printFarmInventory(7, 16, 3);
 }
