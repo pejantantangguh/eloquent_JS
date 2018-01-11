20b83401a2d852c9443712076bfdf2dc13f1d619
Pejantantangguh <suherman.ng@gmail.com>
2018-01-11 16:04:00 +1000

    Browser.html

--- app/home.js
+++ app/home.js
@@ -36,4 +36,31 @@ console.log(link.href);
 
 //All element nodes have a getElementsByTagName method, which collects all elements with the given tag name that are descendants (direct or indirect children) of the given node and returns them as an array-like object.
 
-var ostrich = document.getElementsById("gertrude");
+//Appending element with JS
+/* The following example defines a utility elt, which creates an element node
+and treats the rest of its arguments as children to that node.
+This function is then used to add a simple attribution to a quote. */
+
+function elt(type){
+  var node = document.createElement(type);
+  for (var i = 1; i<arguments.length; i++){
+    var child = arguments[i];
+    if (typeof child == "string")
+      child = document.createTextNode(child);
+      node.appendChild(child);
+  }
+  return node;
+}
+
+document.getElementById("quote").appendChild(
+  elt("footer", "-",
+    elt("strong","Karl Popper"),
+    ", preface to the second condition of ",
+      elt("em","The Open Society and Its Enemies"),
+      ", 1950"));
+
+let paras = document.body.getElementsByTagName("p");
+Array.prototype.forEach.call(paras, function(para){
+  if(para.getAttribute("data-classified")=="secret")
+    para.parentNode.removeChild(para);
+})
